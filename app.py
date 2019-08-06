e908185c36a9d7be167e697347304603a56e1ab6
Mustafa Hoda <mustafahoda@mustafas-mbp.appfiniti.com>
2019-08-05 22:17:09 -0500

    back to normal

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
-    return str(os.environ['APP_SETTINGS'])
+    return "Hello World"
 
 @app.route('/<name>')
 def hello_name(name):
