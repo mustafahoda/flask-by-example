83b91987effb1b093f23e2c37612255fc88516f4
Mustafa Hoda <mustafahoda@Mustafas-MacBook-Pro.local>
2019-08-06 22:32:45 -0500

    Setup Redis Queue

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ from collections import Counter
 import re
 import operator
 from stop_words import get_stop_words
+from rq import Queue
+from rq.job import Job
+from worker import conn
+from models import *
 
 
 app = Flask(__name__)
@@ -17,59 +21,77 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+q = Queue(connection=conn)
+
 from models import Result
+
+def count_and_save_words(url):
+
+    errors = []
+
+    try:
+        r = requests.get(url)
+    except:
+        errors.append(
+            "Unable to get URL. Please make sure it's valid and try again."
+        )
+        return {"error": errors}
+
+    # text processing
+    raw = BeautifulSoup(r.text).get_text()
+    nltk.data.path.append('./nltk_data/')  # set the path
+    tokens = nltk.word_tokenize(raw)
+    text = nltk.Text(tokens)
+
+    # remove punctuation, count raw words
+    nonPunct = re.compile('.*[A-Za-z].*')
+    raw_words = [w for w in text if nonPunct.match(w)]
+    raw_word_count = Counter(raw_words)
+
+    # stop words
+    no_stop_words = [w for w in raw_words if w.lower() not in stops]
+    no_stop_words_count = Counter(no_stop_words)
+
+    # save the results
+    try:
+        result = Result(
+            url=url,
+            result_all=raw_word_count,
+            result_no_stop_words=no_stop_words_count
+        )
+        db.session.add(result)
+        db.session.commit()
+        return result.id
+    except:
+        errors.append("Unable to add item to database.")
+        return {"error": errors}
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    r = None
-    errors = []
     results = {}
     if request.method == "POST":
-        # get the url that the user has entered
-        try:
-            url = request.form['url']
-            r = requests.get(url)
-            print(r.text)
-
-        except:
-            errors.append(
-                "Unable to get URL. Please make sure it's a valid and try again!"
-            )
-
-    if r:
-        stops = get_stop_words('en')
-
-        # text processing
-        raw = BeautifulSoup(r.text, 'html.parser').get_text()
-        nltk.data.path.append('./nltk_data/') #set the patch
-        tokens = nltk.word_tokenize(raw)
-        text = nltk.Text(tokens)
-        # remove punctuation, count raw words
-        nonPunct = re.compile('.*[A-Za-z].*')
-        raw_words = [w for w in text if nonPunct.match(w)]
-        raw_words_count = Counter(raw_words)
-        #stop words
-        no_stop_words = [w for w in raw_words if w.lower() not in stops]
-        no_stop_words_count = Counter(no_stop_words)
-        # save the results
-        results = sorted(
-            no_stop_words_count.items(),
-            key=operator.itemgetter(1),
-            reverse=True
+        # get url that the person has entered
+        url = request.form['url']
+        if 'http://' not in url[:7]:
+            url = 'http://' + url
+        job = q.enqueue_call(
+            func=count_and_save_words, args=(url,), result_ttl=5000
         )
+        print(job.get_id())
 
-        try:
-            result = Result(
-                url = url,
-                result_all = raw_words_count,
-                result_no_stop_words = no_stop_words_count
-            )
-            db.session.add(result)
-            db.session.commit()
-        except:
-            errors.append("Unable to add items to database")
+    return render_template('index.html', results=results)
 
-    return render_template('index.html', errors=errors, results=results)
+@app.route("/results/<job_key>", methods=['GET'])
+def get_results(job_key):
+
+    job = Job.fetch(job_key, connection=conn)
+
+    if job.is_finished:
+        return str(job.result), 200
+
+    else:
+        return "Nay!", 202
 
 if __name__ == "__main__":
     app.run()
